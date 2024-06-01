d31243f1a813f4b929f25a1f35f7bbf519a42394
aneesa04 <aneesafatima238@gmail.com>
2024-06-01 20:30:37 +0530

    fetching articles data and designing the page

--- src/components/PlanetaryInfo.jsx
+++ src/components/PlanetaryInfo.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {SearchBar} from '.'
+import {useGetArticlesByCategoryQuery} from '../services/astronomicalObjects';
+import astronauts from '../assets/trial.jpg';
 
 function PlanetaryInfo() {
+let first;
+  const {data, error, isFetching} = useGetArticlesByCategoryQuery("planet");
+
   return (
-    <div className='bg-black h-screen'> 
+    !isFetching && (
+    <div className='bg-black h-screen px-10'> 
       <SearchBar/>
+     <div className='flex items-start pt-10'>
+     <div className='w-96 space-y-7'>
+     <h3 className='text-white text-5xl leading-normal'>
+     {data?.collection?.items[0]?.data[0].title}
+      </h3>
+      <p className='text-[#808080] text-xs leading-5'>
+   {   data?.collection?.items[0]?.data[0].description.substring(0,800)}
+      </p>
+      <ul className='text-[#808080] flex text-xs pr-8'>
+        <li className='border-r-[1px] border-[#808080] px-5'>Space and Universe</li>
+        <li className='border-r-[1px] border-[#808080] px-5 '>{data?.collection?.items[0]?.data[0].photographer}</li>
+        <li className='px-5 '>Date</li>
+      </ul>
+      
+     </div>
+     <img src={data?.collection?.items[0].links[0].href} className="w-96 h-60" alt="" />
+     
+     </div> 
     </div>
+    )
   )
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,20 @@
-import React , {useState} from 'react';
+import React , {useEffect, useState} from 'react';
 import { CiSearch } from "react-icons/ci";
 
 function SearchBar() {
-    const [searchTerm , setSearchTerm] = useState("")
+    const [searchTerm , setSearchTerm] = useState("");
+    useEffect(() => console.log(searchTerm), [searchTerm])
   return ( 
-    <div className='pt-7 flex justify-evenly text-[#808080] text-xs font-PtSans tracking-widest'>
+    <div className='pt-7 flex justify-evenly text-[#808080] text-xs font-PtSans tracking-widest items-center'>
      
-    <ul className=' flex justify-center space-x-10'>
+    <ul className=' flex justify-center space-x-10 '>
         <li>All</li>
-      <li>Planets</li>
-      <li>Stars</li>
-      <li>Galaxies</li>
-      <li>Nebulas</li>
+      <li className="cursor-pointer" onClick={() => setSearchTerm("planet")}>Planets</li>
+      <li className="cursor-pointer" onClick={() => setSearchTerm("Star")}>Stars</li>
+      <li className="cursor-pointer" onClick={() => setSearchTerm("Galaxies")}>Galaxies</li>
+      <li className="cursor-pointer" onClick={() => setSearchTerm("Nebula")}>Nebulas</li>
         </ul>  
-<div className='border-b-[1px] flex items-center border-[#808080] pb-3 px-3 '> <CiSearch/><input type="text" placeholder={`search to explore`} className='bg-transparent text-[#808080] outline-0 w-52 px-3' onClick={(e) => setSearchTerm(e.target.value)}></input></div>        
+<div className='border-b-[1px] flex items-center  border-[#808080] pb-2 px-3 '> <CiSearch/><input type="text" placeholder={`search to explore`} className='bg-transparent text-[#808080] outline-0 w-52 px-3' onClick={(e) => setSearchTerm(e.target.value)}></input></div>        
     </div>
   )
 }
