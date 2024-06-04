b6c12a7954e1dd71debd967e48a419ff288354ab
aneesa04 <aneesafatima238@gmail.com>
2024-06-04 21:06:23 +0530

    added no results functionality

--- src/components/PlanetaryInfo.jsx
+++ src/components/PlanetaryInfo.jsx
@@ -1,31 +1,44 @@
 import React, {useEffect, useState} from 'react'
 import {SearchBar, ArticleItems} from '.'
 import {useGetArticlesByCategoryQuery} from '../services/astronomicalObjects';
-import astronauts from '../assets/trial.jpg';
+import {MoonLoader} from "react-spinners/MoonLoader";
+import { MdOutlineSearchOff } from "react-icons/md";
+
 
 function PlanetaryInfo() {
   const [searchTerm , setSearchTerm] = useState("");
 const [coverArticleIndex, setCoverArticleIndex] = useState(0);
   const {data, isFetching} = useGetArticlesByCategoryQuery(searchTerm);
   const [coverArticle, setCoverArticle] = useState("");
+  const [isloading, setIsLoading] = useState(true)
   useEffect(()=>{
     setCoverArticle(data?.collection?.items[0]);
-   
   },[data])
+
+//   if(isFetching) return  <MoonLoader
+//   color="gray"
+//   loading={!isloading}
+//   size={100}
+//   aria-label="Loading Spinner"
+//   data-testid="loader"
+// />
+
+console.log(data)
+
 
  
   return (
-    !isFetching && 
-     
+   !isFetching && 
     <div className='bg-black h-screen px-5 overflow-hidden transition-all'> 
       <SearchBar setSearchTerm={setSearchTerm}/>
+      {!isFetching && data.collection.items.length === 0 && <div className="h-[90vh] flex justify-center items-center bg-transparent text-[#808080] text-[2rem]">No results<MdOutlineSearchOff className='mx-3' color="#808080"/></div>}
       <div className="flex pt-10 ">
      <div className='flex mr-20 pr-4 items-start flex-col w-[70%]' >
      <div className='w-fit flex items-center space-x-3'>
      <h3 className='text-white text-5xl font-light leading-[1.3] w-[400px]'>
      {coverArticle?.data[0]?.title}
       </h3>
-      <img src={coverArticle?.links[0].href} className="w-[390px] h-72 object-cover" alt="" />
+      <img src={coverArticle?.links[0]?.href} className="w-[390px] h-72 object-cover" alt="" />
      </div>
      <div className='w-full space-y-5'>
        <p className='text-[#808080] text-xs leading-5 mt-5'>
