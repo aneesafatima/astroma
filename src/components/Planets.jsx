f58fff7102c22f07a38908f5e70f2ad72518c594
aneesa04 <aneesafatima238@gmail.com>
2024-06-09 12:10:35 +0530

    designed the planets page

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
-import marsBg from '../assets/mars-3d-bg.jpg'
+import React, { useEffect } from 'react'
+import marsBg from '../assets/mars-3d-bg.jpg';
+// import { FaStarOfLife } from "react-icons/fa";
+
 
 function Planets() {
+
+   setTimeout(()=>{
+   document.getElementById("line").style.width = "100%";
+   }, 800)
+
+   
+   const handleMouseEnter = () => {
+    document.getElementById('planet').style.height = "100%"
+    document.getElementById('planet').classList.remove("text-shadow");
+    document.getElementById('planet').style.color = "transparent";
+    
+   }
+   const handleMouseleave = () => {
+   document.getElementById('planet').style.height = "0"
+   document.getElementById('planet').classList.add("text-shadow");
+    document.getElementById('planet').style.color = "black";
+   }
+
   return (
-    <div className='h-screen bg-plants-container-bg relative overflow-hidden'>
-      <div className='planet-container mix-blend-screen '>
+    <div className='h-screen bg-plants-container-bg relative overflow-hidden transition-all pt-10 px-10 '>
+     <div><span className=' bg-[#FFE4B5] w-[0%] block h-[0.4px] transition-all duration-[2000ms]' id='line'></span></div> 
+      < div className=' '>     
+      <h1 className="font-space-age text-8xl absolute  bg-[#FFE4B5]  cursor-default  bg-clip-text  h-0  transition-all duration-[1000ms] text-shadow" id='planet'>Mars
+         </h1>
+
+
+        <button className="px-3 py-1 hover:text-black  text-[#FFE4B5]  rounded-[10px]  border-[1px] hover:scale-95  active:scale-95 border-[#FFE4B5]  relative z-10 text-xs font-bold font-roboto  after:w-full after:content-[''] after:absolute after:z-0 after:top-full after:left-0 after:bg-[#FFE4B5] hover:after:top-0  after:h-0 hover:after:h-full overflow-hidden after:transition-all after:duration-1000" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseleave}>Explore Surface</button>
+      </div>
+     
         <img src={marsBg} className=" w-[800px] h-[600px]  absolute left-1/2  -bottom-[60%] -translate-x-1/2  rounded-t-[600px] shadow-planet-shadow 
     animate-rotatePlanet" alt="planet" />
      
-      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ export default {
       fontFamily: {
         lato: ["lato", "sans-serif"],
         roboto: ["Roboto", "sans-serif"],
-        PtSans : ["PT Sans", "sans-serif"]
+        PtSans: ["PT Sans", "sans-serif"],
+        "space-age": ["Space Age", "sans-serif"],
       },
       screens: {
         xxs: "400px",
@@ -21,29 +22,47 @@ export default {
         "homepage-bg": "url('src/assets/astroma-bg.jpg')",
         "custom-gradient":
           "linear-gradient(360deg, rgba(27,27,28,0.730550284629981) 0%, rgba(223,216,216,1) 100%)",
-          "plants-container-bg" : "radial-gradient(circle at center, #000 30%, #111 70%, #000 100%)"
-
+        "plants-container-bg":
+          "radial-gradient(circle at center, #000 30%, #111 70%, #000 100%)",
       },
       colors: {
         cardsCustomGray: "rgba(31, 29, 29, 0.406)",
       },
-      boxShadow  : {
-        "planet-shadow" :  "0 0 60px rgba(255, 153, 0, 0.6), 0 0 120px rgba(255, 153, 0, 0.4), 0 0 180px rgba(255, 153, 0, 0.2)",
+      boxShadow: {
+        "planet-shadow":
+          "0 0 60px rgba(255, 153, 0, 0.6), 0 0 120px rgba(255, 153, 0, 0.4), 0 0 180px rgba(255, 153, 0, 0.2)",
       },
       keyframes: {
         rotate: {
-         from : {
-          backgroundPosition : "0 0"
-         },
-          to : {
-            backgroundPosition: "-8192px 0"
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-8192px 0",
           },
         },
         animation: {
           rotatePlanet: "rotate 15s linear infinite",
-        }
-      }
+        },
+      },
+      transitionProperty: {
+        top: "top",
+      },
+      dropShadow: {
+        "white-outline":
+          "0 0 0 white, 0 0 0.5rem white, 0 0 1rem white, 0 0 2rem white",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".text-shadow": {
+          "text-shadow":
+            "0 0 0 #FFE4B5,  -1px -1px 0 #FFE4B5, 1px -1px 0 #FFE4B5,-1px 1px 0 #FFE4B5, 1px 1px 0 #FFE4B5",
+            
+        },
+      });
+    },
+  ],
 };
