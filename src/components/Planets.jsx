80dcbf652bb26294384a289d2837bb7a849b2439
aneesa04 <aneesafatima238@gmail.com>
2024-06-12 00:39:12 +0530

    fixing tailwind dynamic classes issue

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -5,31 +5,21 @@ import venusBg from '../assets/venus-bg.jpg';
 
 import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from "react-icons/md";
 
-
-
-
 function Planets() {
    const planets = [{name : 'mercury', bgColor : "#DCDCDC", textColor : "#fff",image : mercuryBg },
-     {name :'venus', bgColor : "#FFA500", textColor : "#FFD700", image: venusBg}, 
-     'earth', 
+     {name :'venus', bgColor : "#FFA500", textColor : "#FFD700", image: venusBg},  
      {name: 'Mars', bgColor : "#FF7F50", textColor : "#FFE4B5", image : marsBg}, 
      'jupiter',
       'saturn', 
       'uranus',
        'neptune'] 
-   const [isReady, setIsReady] = useState(false);
-  const [currentPlanet, setCurrentPlanet] = useState(0)
-  const [colorCombo, setColorCombo] = useState(
-    {
-      lineColor : `bg-[${planets[currentPlanet].textColor}]`,
-    textColor : `text-[${planets[currentPlanet].bgColor}]`,
-    bgColor : `bg-[${planets[currentPlanet].bgColor}]`,
-    buttonColor : `text-[${planets[currentPlanet].textColor}]`,
-    buttonBorder : `border-[${planets[currentPlanet].textColor}]`
-        }
-  )
-   
-  console.log(colorCombo)
+  const [currentPlanet, setCurrentPlanet] = useState(1);
+  const textShadow = "0 0 0 #,  -1px -1px 0 #, 1px -1px 0 #,-1px 1px 0 #, 1px 1px 0 #".replace(/#/g, planets[currentPlanet].textColor)
+  const colorCombo = {
+    lineColor : "",
+    textColor: ""
+  }
+  
  
    const planetAfter = () =>{
    
@@ -61,18 +51,18 @@ function Planets() {
 
   return (
 
-      <div className={`h-screen pt-10 px-10 ${colorCombo.bgColor} overflow-hidden`}>
+      <div className="h-screen pt-10 px-10 overflow-hidden" style={{backgroundColor: planets[currentPlanet].bgColor}}>
         <MdOutlineKeyboardArrowRight color={`${planets[currentPlanet].textColor}`} size={30} className='absolute top-1/2 -translate-y-1/2 right-8 cursor-pointer hover:scale-95' />
         <MdOutlineKeyboardArrowLeft color={`${planets[currentPlanet].textColor}`}   size={30} className='absolute top-1/2 -translate-y-1/2 left-8 cursor-pointer hover:scale-95'/>
   
   
-       <div><span className={`${colorCombo.lineColor} w-[0%] block h-[0.4px] transition-all duration-[2000ms]`} id='line'></span></div> 
+       <div><span className="w-[0%] block h-[0.4px] transition-all duration-[2000ms]" id='line' style={{backgroundColor: planets[currentPlanet].textColor}}></span></div> 
         < div className='relative flex justify-center items-center h-[90vh]'>     
-        <h1 className={`font-space-age text-9xl w-0 opacity-0 absolute left-1/2 trasition-all duration-1000 -translate-x-1/2 ${colorCombo.textColor}   cursor-default  bg-clip-text  h-0  transition-all text-shadow top-14 tracking-tighter` } id='planet'>{planets[currentPlanet].name}
+        <h1 className="font-space-age text-9xl w-0 opacity-0 absolute left-1/2 trasition-all duration-1000 -translate-x-1/2 cursor-default  bg-clip-text  h-0  transition-all top-14 tracking-tighter" style={{color: planets[currentPlanet].bgColor, textShadow}} id='planet'>{planets[currentPlanet].name}
            </h1>
   
   
-          <button className={`px-3 py-1 hover:${colorCombo.textColor}  ${colorCombo.buttonColor} rounded-[10px]  border-[1px]  ${colorCombo.buttonBorder}   hover:scale-95  active:scale-95 relative z-10 text-xs font-bold font-roboto  after:w-full after:content-[''] after:absolute after:z-[-1] after:top-full after:left-0 after:${colorCombo.lineColor} hover:after:top-0  after:h-0 hover:after:h-full overflow-hidden after:transition-all after:duration-700`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseleave}>Explore Surface</button>
+          <button className={`px-3 py-1 hover:${colorCombo.textColor} rounded-[10px]  border-[1px]    hover:scale-95  active:scale-95 relative z-10 text-xs font-bold font-roboto  after:w-full after:content-[''] after:absolute after:z-[-1] after:top-full after:left-0 after:${colorCombo.lineColor} hover:after:top-0  after:h-0 hover:after:h-full overflow-hidden after:transition-all after:duration-700`} style={{color: planets[currentPlanet].textColor, border: `1px solid ${planets[currentPlanet].textColor}`}} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseleave}>Explore Surface</button>
         </div>
        
        <div className=''>
@@ -82,10 +72,7 @@ function Planets() {
       <div className="w-[800px] h-[800px] absolute left-1/2  -bottom-[80%] -translate-x-1/2   rounded-[50%]
          bg-gradient-to-r from-transparent via-transparent to-black"></div>
   </div>
-     
          </div>
-       
-       
       )
 
 
