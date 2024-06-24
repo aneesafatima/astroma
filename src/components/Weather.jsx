64330f37d9b2450b007fd2eebab4d960317cd4cd
aneesa04 <aneesafatima238@gmail.com>
2024-06-24 17:34:25 +0530

    styling the weather data

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,114 +1,135 @@
 import React from "react";
-import { FaLongArrowAltDown, FaLongArrowAltUp } from "react-icons/fa";
-import { useGetWeatherQuery } from "../services/Weather";
+import { FaLongArrowAltDown, FaLongArrowAltUp, FaWind } from "react-icons/fa";
+import { useGetWeatherQuery, useGetUvIndexQuery } from "../services/Weather";
+import { MdOutlineDescription } from "react-icons/md";
+import { BsArrowUpLeftCircle } from "react-icons/bs";
+import { WiSunrise, WiSunset } from "react-icons/wi";
+import { IoMdSunny } from "react-icons/io";
+import { FaArrowTrendUp, FaDroplet } from "react-icons/fa6";
 
-function Weather({geoData}) {
-  const {data, isFetching} = useGetWeatherQuery({lat : geoData?.lat, lng : geoData?.lng },{
-    skip : !geoData?.lng || geoData?.lng
-  })
- console.log(data)
+function Weather({ geoData }) {
+  const { data, isFetching } = useGetWeatherQuery(
+    { lat: geoData?.lat, lng: geoData?.lng },
+    {
+      skip: !geoData?.lat || !geoData?.lng,
+    }
+  );
+  const { data: uvIndex, isFetching: isLoading } = useGetUvIndexQuery(
+    { lat: geoData?.lat, lng: geoData?.lng },
+    {
+      skip: !isFetching,
+    }
+  );
+  console.log(data);
+  console.log(uvIndex);
 
   return (
-     
-    <div className="bg-green-400 h-screen flex flex-col justify-center font-lato">
-        {!isFetching && (<><section className="flex items-center justify-betwwen px-8 bg-red-200">
-        <div className="flex items-center bg-pink-400">
-          <img
-            src="http://openweathermap.org/img/wn/10d@2x.png"
-            className="w-[300px] h-[300px] "
-            alt="weather icon"
-          />
-          <h2 className="text-8xl font-extrabold font-lato">{`26\u00B0`}</h2>
-        </div>
-
-        <div className="temp-container flex w-40 justify-around">
-          <div className="min flex items-center ">
-            <FaLongArrowAltDown />
-            <div className="mx-3">
-              <span className="block text-[#7e7c7c] text-xs">Min</span>
-              {`24\u00B0`}
+    <div className=" h-screen flex flex-col justify-center items-center font-lato bg-black">
+      {!isFetching && !isLoading && (
+        <>
+          <section className="flex items-center flex-col bg-green-400 py-5">
+            <div className="flex items-center">
+              <img
+                src={`http://openweathermap.org/img/wn/${data?.weather[0].icon}@2x.png`}
+                className="w-[200px] h-[200px] "
+                alt="weather icon"
+              />
+              <h2 className="text-8xl font-extrabold font-lato">{`${Math.round(data?.main.temp - 273.15)}\u00B0 C`}</h2>
             </div>
-          </div>
-          <div className="max flex items-center">
-            <FaLongArrowAltUp />
-            <div className="mx-3">
-              <span className="block text-[#7e7c7c] text-xs">Max</span>
-              {`24\u00B0`}
+
+            <div className="temp-container flex w-40 justify-around">
+              <div className="min flex items-center ">
+                <FaLongArrowAltDown />
+                <div className="mx-3">
+                  <span className="block text-[#7e7c7c] text-xs">Min</span>
+                  {`${Math.round(data?.main.temp_min - 273.15)}\u00B0 C`}
+                </div>
+              </div>
+              <div className="max flex items-center">
+                <FaLongArrowAltUp />
+                <div className="mx-3">
+                  <span className="block text-[#7e7c7c] text-xs">Max</span>
+                  {`${Math.round(data?.main.temp_max - 273.15)}\u00B0 C`}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </section>
+          </section>
 
-<section className="flex weather-info">
+          <section className="flex weather-info py-8 px-20 bg-blue-400">
+            <div className="left-section bg-yellow-300 grid grid-cols-2 gap-5 px-5  w-full  ">
+              <div className="rain flex items-center">
+                <MdOutlineDescription size={20} style={{ color: "gray" }} />
+                <div className="mx-3">
+                  <span className="block text-[#7e7c7c] text-xs">
+                    Description
+                  </span>
+                  {data?.weather[0].description}
+                </div>
+              </div>
+              <div className="wind flex items-center">
+                <BsArrowUpLeftCircle size={20} />
+                <div className="mx-3">
+                  <span className="block text-[#7e7c7c] text-xs">Wind</span>
+                  {Math.round(data?.wind.speed * 3.6)} km/h
+                </div>
+              </div>
+              <div className="uv-index flex items-center">
+                <IoMdSunny style={{ color: "yellow" }} size={20} />
+                <div className="mx-3">
+                  <span className="block text-[#7e7c7c] text-xs">UV Index</span>
+                  1.2
+                </div>
+              </div>
+              <div className="pressure flex items-center">
+                <FaArrowTrendUp style={{ color: "lightpink" }} size={20} />
+                <div className="mx-3">
+                  <span className="block text-[#7e7c7c] text-xs">Pressure</span>
+                  {data?.main.pressure} hPa
+                </div>
+              </div>
+            </div>
 
-<div className="left-section grid grid-cols-2 gap-5 bg-orange-200 w-full py-5 px-20 space-y-5">
-       
-       <div className="rain flex items-center">
-         <FaLongArrowAltDown />
-         <div className="mx-3">
-           <span className="block text-[#7e7c7c] text-xs">Chance of rain</span>
-           0%
-         </div>
-       </div>
-       <div className="wind flex items-center">
-         <FaLongArrowAltDown />
-         <div className="mx-3">
-           <span className="block text-[#7e7c7c] text-xs">Wind</span>
-           300 km/h
-         </div>
-       </div>
-       <div className="uv-index flex items-center">
-         <FaLongArrowAltDown />
-         <div className="mx-3">
-           <span className="block text-[#7e7c7c] text-xs">UV Index</span>
-           1.2
-         </div>
-       </div>
-       <div className="pressure flex items-center">
-         <FaLongArrowAltDown />
-         <div className="mx-3">
-           <span className="block text-[#7e7c7c] text-xs">Pressure</span>
-           952 hPa
-         </div>
-       </div>
-     </div>
-     <div className="right-section grid grid-cols-2 gap-5 bg-blue-300 w-full">
-       <div className="rain flex items-center">
-         <FaLongArrowAltDown />
-         <div className="mx-3">
-           <span className="block text-[#7e7c7c] text-xs">Chance of rain</span>
-           0%
-         </div>
-       </div>
-       <div className="wind flex items-center">
-         <FaLongArrowAltDown />
-         <div className="mx-3">
-           <span className="block text-[#7e7c7c] text-xs">Wind</span>
-           300 km/h
-         </div>
-       </div>
-       <div className="uv-index flex items-center">
-         <FaLongArrowAltDown />
-         <div className="mx-3">
-           <span className="block text-[#7e7c7c] text-xs">UV Index</span>
-           1.2
-         </div>
-       </div>
-       <div className="pressure flex items-center">
-         <FaLongArrowAltDown />
-         <div className="mx-3">
-           <span className="block text-[#7e7c7c] text-xs">Pressure</span>
-           952 hPa
-         </div>
-       </div>
-     </div>
-
-
-    
-</section></>)}
-      
-     
-   
+            <div className="right-section bg-red-500 grid grid-cols-2 px-5 gap-5 w-full">
+              <div className="rain flex items-center ">
+                <WiSunrise size={20} style={{ color: "yellow" }} />
+                <div className="mx-3">
+                  <span className="block text-[#7e7c7c] text-xs">Sunrise</span>
+                  {new Date(data?.sys.sunrise * 1000).toLocaleTimeString(
+                    "en-US",
+                    { hour: "numeric", minute: "numeric" }
+                  )}
+                </div>
+              </div>
+              <div className="wind flex items-center">
+                <WiSunset size={20} style={{ color: "orange" }} />
+                <div className="mx-3">
+                  <span className="block text-[#7e7c7c] text-xs">Sunset</span>
+                  {new Date(data?.sys.sunset * 1000).toLocaleTimeString(
+                    "en-US",
+                    { hour: "numeric", minute: "numeric" }
+                  )}
+                </div>
+              </div>
+              <div className="uv-index flex items-center">
+                <FaDroplet size={20} style={{ color: "lightblue" }} />
+                <div className="mx-3">
+                  <span className="block text-[#7e7c7c] text-xs">Humidity</span>
+                  {data?.main.humidity}%
+                </div>
+              </div>
+              <div className="pressure flex items-center">
+                <FaWind size={20} style={{ color: "lightgreen" }} />
+                <div className="mx-3">
+                  <span className="block text-[#7e7c7c] text-xs">
+                    Wind direction
+                  </span>
+                  {`${data?.wind.deg}\u00B0`}
+                </div>
+              </div>
+            </div>
+          </section>
+        </>
+      )}
     </div>
   );
 }
--- src/services/Weather.js
+++ src/services/Weather.js
@@ -2,12 +2,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const weatherApi = createApi({
  reducerPath: 'weatherApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://images-api.nasa.gov' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.openweathermap.org/data/2.5' }),
   endpoints: (builder) => ({
     getWeather: builder.query({
-      query: ({lat, lng}) => `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${import.meta.env.VITE_REACT_APP_WEATHER_API}`,
+      query: ({lat, lng}) => `/weather?lat=${lat}&lon=${lng}&appid=${import.meta.env.VITE_REACT_APP_WEATHER_API}`,
     }),
+    getUvIndex: builder.query({
+      query: ({lat, lng}) => `/uvi?lat=${lat}&lon=${lng}&appid=${import.meta.env.VITE_REACT_APP_WEATHER_API}`,
+    })
   }),
 })
 
-export const {useGetWeatherQuery} = weatherApi;
+export const {useGetWeatherQuery, useGetUvIndexQuery} = weatherApi;
