5973b80052caca8974b5ef547055b5c9e89ed0de
aneesa04 <aneesafatima238@gmail.com>
2024-09-26 16:09:13 +0530

    fixing styling

--- src/components/HomeIcon.jsx
+++ src/components/HomeIcon.jsx
@@ -7,7 +7,7 @@ function HomeIcon() {
     const location = useLocation();
     if(location.pathname === "/") return;
   return (
-    <IoHome className="absolute z-50 text-[#f2f2f2] left-7 top-5 cursor-pointer hover:text-[#edecec]" onClick={() => navigate("/")}/>
+    <IoHome className="absolute z-50 text-[#f2f2f2] left-3 xs:left-4 top-5 cursor-pointer hover:text-[#edecec]" onClick={() => navigate("/")}/>
   )
 }
 
